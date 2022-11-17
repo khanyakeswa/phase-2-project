dcb27d8fadc1470da24c3ccaebac1237b0a69ea3
Khanya <khanya.keswa@gmail.com>
2022-11-17 12:51:02 -0600

    container styling updated

--- src/Components/CharacterCollection.js
+++ src/Components/CharacterCollection.js
@@ -1,25 +1,31 @@
-import { React } from "react";
+import { React } from 'react'
 
-const CharacterCollection = ({setSelectedCharacter, charactersData}) => {
+const CharacterCollection = ({ setSelectedCharacter, charactersData }) => {
+  const tileClickHandler = (e) => {
+    setSelectedCharacter(charactersData[e.target.id - 1])
+  }
 
-    const tileClickHandler = (e) => {
-        setSelectedCharacter(charactersData[e.target.id - 1])
-    }
-
-    const characterTiles = charactersData.map((obj) => {
-        return (
-            <div id={obj.id} key={obj.id} onClick={tileClickHandler} className="tile-container">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"></img>
-                <span>{obj.name}</span>
-            </div>
-        )
-    })
-
+  const characterTiles = charactersData.map((obj) => {
     return (
-    <div id="character-collection-container">
-        {characterTiles}
-    </div>
+      <div
+        id={obj.id}
+        key={obj.id}
+        onClick={tileClickHandler}
+        className='tile-container'
+      >
+        <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'></img>
+        <span>{obj.name}</span>
+      </div>
     )
+  })
+
+  return (
+    <div id='character-collection-wrapper'>
+      <div id='character-collection-container' className=''>
+        {characterTiles}
+      </div>
+    </div>
+  )
 }
 
 export default CharacterCollection
