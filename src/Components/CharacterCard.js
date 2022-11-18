2572ce57b9c65323243fb5b1ee3bfed0550671fa
Khanya <khanya.keswa@gmail.com>
2022-11-17 20:31:48 -0600

    text-transition on card container added

--- src/Components/CharacterCard.js
+++ src/Components/CharacterCard.js
@@ -14,13 +14,15 @@ function CharacterCard({ selectedCharacter }) {
       <div id='character-card'>
         <img src='https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'></img>
         <div id='details-container'>
-          <TextTransition
-            direction='down'
-            springConfig={presets.gentle}
-            style={{ justifyContent: 'center', height: 36.5 }}
-          >
-            <span>{selectedCharacter.name}</span>
-          </TextTransition>
+          <div id='card-name-container'>
+            <TextTransition
+              direction='down'
+              springConfig={presets.gentle}
+              style={{ justifyContent: 'center', height: 36.5 }}
+            >
+              <span>{selectedCharacter.name}</span>
+            </TextTransition>
+          </div>
           <span></span>
           <p>Description</p>
         </div>
