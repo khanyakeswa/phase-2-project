72859ec8b833fbc251e2d446c1b2024036d5ef32
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-11-17 21:15:50 -0500

    Added click logic for abilities.

--- src/Components/Forms/DetailsForm.js
+++ src/Components/Forms/DetailsForm.js
@@ -1,49 +1,58 @@
 import React, { useState } from "react";
 // import AvatarCarousel from "../AvatarCarousel";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
 function DetailsForm() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const [name, setName] = useState('');
-  const [roles, setRoles] = useState('');
+  const [name, setName] = useState("");
+  const [roles, setRoles] = useState("");
 
-  const handleNameChange = e => {
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
 
-    setName(e.target.value)
-  }
-
-  const handleRolesChange = e => { 
-
-    setRoles(e.target.value)
-  }
+  const handleRolesChange = (e) => {
+    setRoles(e.target.value);
+  };
 
   return (
     <div>
-      {/* <AvatarCarousel /> */}
-
-      <form>
-        <label style={{ color:'white' }} htmlFor="name" id="name">
+        
+      <form id="details-form">
+        <label htmlFor="name" id="name">
           Name Your Character
-          <input type="text" id="name" value={name} onChange={handleNameChange}></input>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={handleNameChange}
+          ></input>
         </label>
-
-        <label style={{ color:'white' }} htmlFor="roles"> Decide Your Role
-          <select id='roles' onChange={handleRolesChange}>
-            <option value=''></option>
-            <option value='barbarian'>Barbarian</option>
-            <option value='bard'>Bard</option>
-            <option value='cleric'>Cleric </option>
-            <option value='druid'>Druid </option>
-            <option value='fighter'>Fighter</option>
-            <option value='monk '>Monk </option>
-            <option value='paladin'>Paladin </option>
+        <br />
+        <label htmlFor="roles">
+          {" "}
+          Decide Your Role
+          <select id="roles" onChange={handleRolesChange}>
+            <option value=""></option>
+            <option value="barbarian">Barbarian</option>
+            <option value="bard">Bard</option>
+            <option value="cleric">Cleric </option>
+            <option value="druid">Druid </option>
+            <option value="fighter">Fighter</option>
+            <option value="monk ">Monk </option>
+            <option value="paladin">Paladin </option>
           </select>
         </label>
 
-        <button className="formsButton" onClick={()=>navigate("/character/create/abilities")}> Create Character</button>
+        <button
+          className="formsButton"
+          onClick={() => navigate("/character/create/abilities")}
+        >
+          {" "}
+          Create Character
+        </button>
       </form>
-
     </div>
   );
 }
